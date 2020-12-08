dbfee57323c697d0b0ca188a442299bd68a3eeda
Aniket <aniketg.vnit@gmail.com>
2020-12-08 16:02:51 +0530

    for front and rear cam

--- robotcar/robotcar-dataset-sdk-3.1/python/getTop_rcar_depth.py
+++ robotcar/robotcar-dataset-sdk-3.1/python/getTop_rcar_depth.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 from project_laser_into_camera import get_uvd
 import argparse
 import os
+import re
 from image import load_image
 
 def plotPts(trgPts):
@@ -28,7 +29,7 @@ if __name__ == '__main__':
 	parser.add_argument('--image_idx', type=int, help='Index of image to display')
 	args = parser.parse_args()
 	#depthFile = argv[2]
-	
+
 	srcPts = []
 	trgPts = []
 
@@ -39,33 +40,35 @@ if __name__ == '__main__':
 
 	image_path = os.path.join(args.image_dir, str(timestamp) + '.png')
 	img = load_image(image_path, model)
-	## Rear points
-	# bottom left -> bottom right -> top right -> top left
-	pts = [[9, 787], [1276, 801], [768, 465], [460, 462]]
 
-	## Front points
-	# bottom left -> bottom right -> top right -> top left
-	#pts = [[9, 787], [1276, 801], [845, 545], [486, 531]]
+	camera = re.search('(stereo|mono_(left|right|rear))', args.image_dir).group(0)
+
+	if camera == 'mono_rear':
+		## Rear points
+		# bottom left -> bottom right -> top right -> top left
+		pts = [[9, 787], [1276, 801], [768, 465], [460, 462]]
+		## Rear camera intrinsics
+		#scalingFactor = 100.0
+		focalLength = 400.000000
+		centerX = 508.222931
+		centerY = 498.187378
+	elif camera == 'stereo':
+		## Front points
+		# bottom left -> bottom right -> top right -> top left
+		pts = [[9, 787], [1276, 801], [845, 545], [486, 531]]
+		## Front camera intrinsics
+		#scalingFactor = 100.0
+		focalLength = 964.828979
+		centerX = 643.788025
+		centerY = 484.407990
 
 	rgb = cv2.cvtColor(np.array(img), cv2.COLOR_BGR2RGB)
 	rgb = cv2.resize(rgb, (1280, 960))
 	for i in range(0, len(pts)):
 		rgb = cv2.circle(rgb, (int(pts[i][0]), int(pts[i][1])), 1, (0, 0, 255), 2)
 
-	cv2.imshow("Image", rgb)
-	cv2.waitKey(0)
-
-	# ## Front camera intrinsics
-	# #scalingFactor = 100.0
-	# focalLength = 964.828979
-	# centerX = 643.788025
-	# centerY = 484.407990
-
-	## Rear camera intrinsics
-	#scalingFactor = 100.0
-	focalLength = 400.000000
-	centerX = 508.222931
-	centerY = 498.187378
+	# cv2.imshow("Image", rgb)
+	# cv2.waitKey(0)
 
 	uv_new = []
 	for i in range(uv.shape[1]):
@@ -114,16 +117,11 @@ if __name__ == '__main__':
 	# print(trgPts)
 	# plotPts(trgPts)
 
-	for i in range(0, trgPts.shape[0]):
-		rgb = cv2.circle(rgb, (int(trgPts[i, 0]), int(trgPts[i, 1])), 1, (50, 255, 50), 2)
-	cv2.imshow("Image", rgb)
-	cv2.waitKey(0)
-
 	homographyMat, status = cv2.findHomography(srcPts, trgPts)
 	orgImg = cv2.cvtColor(np.array(img), cv2.COLOR_BGR2RGB)
 	warpImg = cv2.warpPerspective(orgImg, homographyMat, (trgSize, trgSize))
 
 	cv2.imshow("Warped", warpImg)
 	cv2.waitKey(0)
-	cv2.imwrite("/home/udit/d2-net/media/get_TOP/rcar_rear_top.png", warpImg)
-	cv2.imwrite("/home/udit/d2-net/media/get_TOP/rcar_rear.png", rgb)
+	cv2.imwrite("/home/udit/d2-net/media/rcar_samples/1_top.png", warpImg)
+	#cv2.imwrite("/home/udit/d2-net/media/get_TOP/rcar_rear.png", rgb)
